5c67ec4759a9dba082fd54777b49441e4d12cb9b
LostWarrior <kirti.sbhardwaj@gmail.com>
2016-07-20 17:01:12 +0530

    plotting points

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function initMap(sensors){
+function initMap(sensors,styleArray){
 
 	console.log('initmap called');
 
@@ -10,33 +10,83 @@ function initMap(sensors){
 
 	var overlay = new google.maps.OverlayView(); 
 
+	// Create Map //
+
 	map = new google.maps.Map(mapDiv, {
 	  zoom: 18,
+	  styles: styleArray,
 	  center: {lat:51.7063278, lng: -0.612019444444444}
 	});
 
-	for(var i = 0; i < sensors.length; i++){
 
-		var sensor = sensors[i];
+	overlay.onAdd = function(){
 
-		console.log(sensor);
+		var width = 1200;
 
-		var marker = new google.maps.Marker({
-		    map: map,
-		    draggable: true,
-		    animation: google.maps.Animation.DROP,
-		   	title:sensor[0],
-	    	position: {lat: sensors[i][1], lng: sensors[i][2]}
+	   	var height = 500;
 
-	  	});
+		var layer = d3.select(this.getPanes().overlayLayer)
+					  .append("div")
+					  .attr({'width': width, 'height': height})
+        			  .attr("class", "zones");
+
+
+        overlay.draw = function(){
+
+        	var projection = this.getProjection();
+
+        	var padding = 10;
+
+        	var svg = layer.append('svg')
+        				   .attr({'width': width, 'height': height});
+
+        	var marker = svg.selectAll('.marker')
+        					.data(sensors);
+
+        	marker.enter().append('g')
+        		  .attr('class','marker');
+
+
+			marker.append("circle")
+		          .attr("r", 4.5)
+		          .attr("cx", function(d,i){
+		          	var coord = transform(d);
+		          	return coord[0];
+		          })
+		          .attr("cy", function(d,i){
+		          	var coord = transform(d);
+		          	return coord[1];
+		          });
+
+
+		    marker.append('text')
+					.attr("x", function(d,i){
+						var coord = transform(d);
+		          		return coord[0] - padding;
+					})
+					.attr("y",function(d,i){
+						var coord = transform(d);
+		          		return coord[1] + padding * 1.4;
+					})
+					.attr("dy", ".31em")
+					.text(function(d) { 
+						return d[0]; 
+					});
+
+			function transform(d){
+				d = new google.maps.LatLng(d[1], d[2]);
+
+		        d = projection.fromLatLngToDivPixel(d);
+
+		        return [d.x, d.y];
+
+			}
+
+        }
 	}
-
-
-		// google.maps.event.addListener(marker, 'click', (function(marker, i) {
-		//     return function() {
-		//       infowindow.setContent(sensors[i][0]);
-		//       infowindow.open(map, marker);
-		//     }
-		//   })(marker, i));
 	
+
+	overlay.setMap(map);
 }
+
+
